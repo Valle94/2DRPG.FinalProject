9ae4dbefb83f697693632964e716c16d21d3d57c
Max Valle <55932182+Valle94@users.noreply.github.com>
2024-11-18 22:05:17 -0500

    Import Sprites and Create Animations
    Imported some sprites and created animations for the player. Implemented a method for facing the player towards to mouse position at all times.

--- 2D Combat RPG (Final Project)/Assets/Scripts/PlayerController.cs
+++ 2D Combat RPG (Final Project)/Assets/Scripts/PlayerController.cs
@@ -10,11 +10,15 @@ public class PlayerController : MonoBehaviour
     PlayerControls playerControls;
     Vector2 movement;
     Rigidbody2D rb;
+    Animator myAnimator;
+    SpriteRenderer mySpriteRenderer;
 
     void Awake() 
     {
         playerControls = new PlayerControls();
         rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void OnEnable() 
@@ -29,16 +33,35 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
+        AdjustPlayerFacingDirection();
         Move();
     }
 
     void PlayerInput()
     {
         movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
     }
 
     void Move()
     {
         rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else
+        {
+            mySpriteRenderer.flipX = false;
+        }
     }
 }
